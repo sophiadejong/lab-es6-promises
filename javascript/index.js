c71e90fc6da7b9e25dc34f5d1d1d1532fda504eb
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-09-29 16:20:57 +0200

    promiseall

--- javascript/index.js
+++ javascript/index.js
@@ -34,7 +34,23 @@
 // ...
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+
+}
 
 // Bonus 2 - Promise all
-// ...
+const prom1 = obtainInstruction("brusselsSprouts", 0);
+const prom2 = obtainInstruction("brusselsSprouts", 1);
+const prom3 = obtainInstruction("brusselsSprouts", 2);
+const prom4 = obtainInstruction("brusselsSprouts", 3);
+const prom5 = obtainInstruction("brusselsSprouts", 4);
+const prom6 = obtainInstruction("brusselsSprouts", 5);
+const prom7 = obtainInstruction("brusselsSprouts", 6);
+const prom8 = obtainInstruction("brusselsSprouts", 7);
+
+Promise.all([prom1, prom2, prom3, prom4, prom5, prom6, prom7, prom8]).then((values) => {
+  values.forEach((value) => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${value}</li>`;
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+  });
+});
